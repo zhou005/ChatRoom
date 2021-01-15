b4fb66f6d8e5a7d4432b7b4e111c9701ec9e59ac
zhoiu005 <2416383380@qq.com>
2021-01-15 22:01:24 +0800

    未完成

---
+++ src/cn/edu/hcnu/client/Client.java
@@ -0,0 +1,13 @@
+package cn.edu.hcnu.client;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+//聊天室客户端
+public class Client {
+	//主方法:启动登录线程
+	public static void main(String[] args) throws Exception {
+		Thread login = new LoginThread();
+		login.start();
+	}
+}
